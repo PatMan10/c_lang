729b7bcd9bb8006c4a916ccd5ee80acf02602879
PatMan10 <PatMan10@protonmail.com>
2024-03-11 20:16:07 +0200

    progress

---
+++ chapter_01/exercise_1-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * print Fahrenheit to Celsius table
+ */
+
+int main() {
+  float fahr, cels;
+  int lower, upper, step;
+
+  lower = 0; // lower limit of temprature table
+  upper = 300; // upper limit
+  step = 20;  // step size
+
+  fahr = lower;
+  while (fahr <= upper) {
+    cels = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t%6.1f\n", fahr, cels);
+    fahr = fahr + step;
+  }
+
+  return 0;
+}

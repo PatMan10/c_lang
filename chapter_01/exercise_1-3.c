7233a644cabab1a0e11aa96cc1e709825628ac2d
PatMan10 <PatMan10@protonmail.com>
2024-03-11 20:17:43 +0200

    1-3 done

--- chapter_01/exercise_1-3.c
+++ chapter_01/exercise_1-3.c
@@ -13,6 +13,7 @@ int main() {
   step = 20;  // step size
 
   fahr = lower;
+  printf("Fahrenheit to Celsius\n\n");
   while (fahr <= upper) {
     cels = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f\t%6.1f\n", fahr, cels);

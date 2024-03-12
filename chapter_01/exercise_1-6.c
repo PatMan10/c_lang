c0d30f65939d2ea13c8dc272d7791d305af77c85
PatMan10 <PatMan10@protonmail.com>
2024-03-12 20:37:27 +0200

    1-6 done

---
+++ chapter_01/exercise_1-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Character Input and Output
+ */
+
+
+int main() {
+  int c;
+  while ((c = getchar()) != EOF) {
+    putchar(c);
+  }
+  printf("\nc != EOF -> %d\n", c != EOF);
+  return 0;
+}

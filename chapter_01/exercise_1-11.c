524079d2da7dedff0af1d3d7d0cc11e4fc2e763b
PatMan10 <PatMan10@protonmail.com>
2024-03-13 11:30:14 +0200

    1-11 done

---
+++ chapter_01/exercise_1-11.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+ * Count lines, words and characters in input.
+ */
+
+#define IN 1  /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+  int cur, prev, lines, words, chars, state;
+
+  state = OUT;
+  lines = words = chars = 0;
+  prev = ' ';
+  while ((cur = getchar()) != EOF) {
+    ++chars;
+
+    if (cur == '\n')
+      ++lines;
+
+    int cur_is_blank = cur == ' ' || cur == '\t' || cur == '\n';
+    int prev_is_blank = prev == ' ' || prev == '\t' || prev == '\n';
+
+    if (cur_is_blank)
+      state = OUT;
+
+    if (!cur_is_blank && prev_is_blank) {
+      state = IN;      
+      ++words;
+    }
+    prev = cur;
+  }
+  printf("%d chars\n", chars);
+  printf("%d words\n", words);
+  printf("%d lines\n", lines);
+  
+  return 0;
+}

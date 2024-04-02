62df3bd33179e13019f180bba269f46cc030e2b0
PatMan10 <PatMan10@protonmail.com>
2024-04-02 13:31:33 +0200

    1-17 done

--- chapter_01/exercise_1-17.c
+++ chapter_01/exercise_1-17.c
@@ -8,18 +8,32 @@
 #define MIN_LENGTH 3
 
 int read_line(char line[], int max_length);
+void copy(char from[], char to[]);
 
 int main() {
-  const int size = 101;
-  char buffer[size] = {};
-  int length = 0;
+  const int num_lines = 10, line_length = 11;
+  char lines[num_lines][line_length] = {};
+  int lengths[num_lines] = {};
 
-  while ((length = read_line(buffer, size)) > 0) {
-    if (length >= MIN_LENGTH) {
-      printf("%d %s\n", length, buffer);
+  const int size = line_length;
+  char buffer[size] = {};
+
+  int length = 0;
+  int idx = 0;
+  while ((length = read_line(buffer, size)) > 0 && idx < num_lines) {
+    if (length >= MIN_LENGTH ) {
+      copy(buffer, lines[idx]);
+      lengths[idx] = length;
+      ++idx;
     }
   }
 
+  system("clear");
+  printf("%d lines longer than %d characters\n", idx, MIN_LENGTH);
+  for (int i = 0; i < idx; ++i) {
+    printf("%d %s\n", lengths[i], lines[i]);
+  }
+ 
   return 0;
 }
 
@@ -31,4 +45,14 @@ int read_line(char line[], int max_length) {
   }
   line[i] = '\0';
   return i;
+}
+
+// copy characters [from] [to]
+void copy(char from[], char to[]) {
+  int i = 0;
+  while (from[i]) {
+    to[i] = from[i];
+    ++i;
+  }
+  to[i] = '\0';
 }

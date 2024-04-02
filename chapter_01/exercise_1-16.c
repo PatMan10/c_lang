2851fe86651c579ebc9002d2715c351b2d86476d
PatMan10 <PatMan10@protonmail.com>
2024-04-02 12:26:50 +0200

    1-16 base work done

--- chapter_01/exercise_1-16.c
+++ chapter_01/exercise_1-16.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
- * Print Celsius to Fahrenheit table, use function for conversion.
+ * Longest line.
  */
 
-void modify(int nums[], int size) {
-   for (int i = 0; i < 10; ++i) {
-    nums[i] = i * 2;
-  }
-}
+#define MAX_LENGTH 11
+
+int read_line(char line[], int max_length);
+void copy(char from[], char to[]);
 
 int main() {
-  int z = 10;
-  int nums[z] = {};
-  modify(nums, z);
-  for (int i = 0; i < z; ++i) {
-    printf("%d\n", nums[i]);
-  } 
+  char max_line[MAX_LENGTH] = {},
+       cur_line[MAX_LENGTH] = {};
+  int max_length = 0, cur_length, c;
 
-  char *chars = "abcde";
-  // chars[1] = 'X';
-  for (int i = 0; i < 5; ++i) {
-    printf("%c\n", chars[i]);
-  } 
+  while ((cur_length = read_line(cur_line, MAX_LENGTH)) > 0) {
+    if (cur_length >= max_length) {
+      copy(cur_line, max_line);
+      max_length = cur_length;
+    }
+  }
+
+  if (max_length) {
+    system("clear");
+    printf("max length: %d\n", max_length);
+    printf("max line: %s\n", max_line);
+  } else {
+    printf("no lines...\n");
+  }
+
+  return 0;
+}
+
+// read up to [max_length] characters into [line]
+int read_line(char line[], int max_length) {
+  int i, c;
+  for (i = 0; i < max_length - 1 && (c = getchar()) != '\n' && c != EOF; ++i) {
+    line[i] = c;
+  }
+  line[i] = '\0';
+  return i;
+}
+
+// copy characters [from] [to]
+void copy(char from[], char to[]) {
+  int i = 0;
+  while (from[i]) {
+    to[i] = from[i];
+    ++i;
+  }
+  to[i] = '\0';
 }

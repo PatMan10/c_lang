affbfa7d2aac8f2e1523012a7cc56f7ddc8548a1
PatMan10 <PatMan10@protonmail.com>
2024-04-02 15:49:25 +0200

    progress on 1-18

--- chapter_01/exercise_1-18.c
+++ chapter_01/exercise_1-18.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 /*
- * Print lines longer than X.
+ * Remove preceding and trailing white space then print line.
  */
 
-#define MIN_LENGTH 3
-
 int read_line(char line[], int max_length);
+void trim(char from[], char to[]);
 
 int main() {
   const int size = 101;
-  char buffer[size] = {};
+  char buffer[size] = {},
+       trimmed[size] = {};
   int length = 0;
 
   while ((length = read_line(buffer, size)) > 0) {
-    if (length >= MIN_LENGTH) {
-      printf("%d %s\n", length, buffer);
-    }
+    trim(buffer, trimmed);
+    printf("%s\n", trimmed);
   }
 
   return 0;
@@ -31,4 +30,38 @@ int read_line(char line[], int max_length) {
   }
   line[i] = '\0';
   return i;
+}
+
+int blank(char c) {
+  return c == ' ' || c == '\t' || c == '\n';
+}
+
+// copy characters [from] [to], remove preceding and trailing white space
+void trim(char from[], char to[]) {
+  int first = 0, last = 0, i = 0, c;
+
+  // find index of first and last non-blank characters
+  while (c = from[i]) {
+    if (!first && !blank(c)) {
+      first = i;
+    }
+    if (!blank(c)) {
+      last = i;
+    }
+    ++i;
+  }
+
+  i = 0;
+  while (c = from[i]) {
+    if (i >= first && i <= last) {
+      to[i] = c;
+      printf("'%c'", to[i]);
+    }
+    if (i > last) {
+      to[i] = '\0';
+      printf("\nfrom: '%s'\nto: '%s'\n", from, to);
+      return;
+    }
+    ++i;
+  }
 }

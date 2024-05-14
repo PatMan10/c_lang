606c5abbf96c9ca825407c05993bb4d77a5d07a3
PatMan10 <PatMan10@protonmail.com>
2024-05-14 21:18:48 +0200

    ADDED: test for str_len

--- chapter_01/ex-21/test.c
+++ chapter_01/ex-21/test.c
@@ -3,6 +3,12 @@
 #include <criterion/redirect.h>
 
 Test(str_len, passing) {
-  int result = str_len("abc");
+  int result = str_len("");
+  cr_assert_eq(result, 0);
+
+  result = str_len("a");
+  cr_assert_eq(result, 1);
+
+  result = str_len("abc");
   cr_assert_eq(result, 3);
 }

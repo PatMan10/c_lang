84a5fa20141caabd073ede8c6c0594e43096f1f1
PatMan10 <PatMan10@protonmail.com>
2024-05-14 21:37:20 +0200

    ADDED: test for basic functions

--- chapter_01/ex-21/test.c
+++ chapter_01/ex-21/test.c
@@ -2,7 +2,8 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-Test(str_len, passing) {
+// STR_LEN
+Test(str_len, pass) {
   int result = str_len("");
   cr_assert_eq(result, 0);
 
@@ -11,4 +12,61 @@ Test(str_len, passing) {
 
   result = str_len("abc");
   cr_assert_eq(result, 3);
+}
+
+// SPACE
+Test(space, yes) {
+  char c = ' ';
+  cr_assert_eq(space(c), 1);
+}
+
+Test(space, no) {
+  char c = '\t';
+  cr_assert_eq(space(c), 0);
+}
+
+// TAB
+Test(tab, yes) {
+  char c = '\t';
+  cr_assert_eq(tab(c), 1);
+}
+
+Test(tab, no) {
+  char c = ' ';
+  cr_assert_eq(tab(c), 0);
+}
+
+// BLANK
+Test(blank, yes) {
+  char c = ' ';
+  cr_assert_eq(blank(c), 1);
+
+  c = '\t';
+  cr_assert_eq(blank(c), 1);
+}
+
+Test(blank, no) {
+  char c = 'a';
+  cr_assert_eq(blank(c), 0);
+
+  c = 'z';
+  cr_assert_eq(blank(c), 0);
+}
+
+
+// BLANK_SEQ
+Test(blank_seq, yes) {
+  char prev = 'a', cur = ' ';
+  cr_assert_eq(blank_seq(prev, cur), 1);
+
+  prev = '\t', cur = ' ';
+  cr_assert_eq(blank_seq(prev, cur), 1);
+}
+
+Test(blank_seq, no) {
+  char prev = ' ', cur = 'a';
+  cr_assert_eq(blank_seq(prev, cur), 0);
+
+  prev = 'a', cur = 'b';
+  cr_assert_eq(blank_seq(prev, cur), 0);
 }
